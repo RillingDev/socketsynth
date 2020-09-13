57da316c3936da35e43a85caded7b7446b6edbc8
FelixRilling <contact@rilling.dev>
2020-09-13 20:18:05 +0200

    Lazy init audio ctx due to chrome blocking it without interactive access.

--- src/main/ts/audio/audioCtx.ts
+++ src/main/ts/audio/audioCtx.ts
@@ -1,9 +1,14 @@
-const audioContext = new AudioContext();
-
-const masterGainNode = audioContext.createGain();
-masterGainNode.connect(audioContext.destination);
+let audioContext: AudioContext | null = null;
+let masterGainNode: GainNode | null = null;
 
 const createOsc = (): OscillatorNode => {
+    // Lazy init on interactive usage because otherwise chrome blocks ctx creation.
+    if (audioContext == null || masterGainNode == null) {
+        audioContext = new AudioContext();
+        masterGainNode = audioContext.createGain();
+        masterGainNode.connect(audioContext.destination);
+    }
+
     const osc = audioContext.createOscillator();
     osc.connect(masterGainNode);
     return osc;
