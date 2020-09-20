cbe5454f4e90f92b5e327880b5cf8fa8bebeef7a
FelixRilling <contact@rilling.dev>
2020-09-20 11:00:17 +0200

    Adjusted oscs.

--- src/main/ts/audio/oscMods.ts
+++ src/main/ts/audio/oscMods.ts
@@ -10,34 +10,52 @@ export const sineOscMod: OscModifier = (
     osc.connect(gainNode);
 };
 
+// Based on https://itnext.io/algorithmic-reverb-and-web-audio-api-e1ccec94621a
+const createReverbNode = (
+    audioCtx: AudioContext,
+    delayTime: number,
+    delayWet: number,
+    lpCutoff: number
+): AudioNode => {
+    const delay = new DelayNode(audioCtx, { delayTime: delayTime });
+
+    const lowPass = new BiquadFilterNode(audioCtx, {
+        type: "lowpass",
+        frequency: lpCutoff,
+    });
+
+    const gain = audioCtx.createGain();
+    gain.gain.setValueAtTime(delayWet, audioCtx.currentTime);
+
+    const node = audioCtx.createGain();
+    node.connect(delay).connect(lowPass).connect(gain).connect(node);
+    return node;
+};
+
 export const leadOscMod: OscModifier = (osc, audioCtx, gainNode) => {
     osc.type = "sawtooth";
 
-    const panner = new PannerNode(audioCtx, {
-        panningModel: "HRTF",
-        distanceModel: "linear",
+    const reverb = createReverbNode(audioCtx, 0.5, 0.275, 800);
 
-        refDistance: 1,
-        maxDistance: 10000,
-        rolloffFactor: 1,
-        coneInnerAngle: 360,
-        coneOuterAngle: 0,
-        coneOuterGain: 0,
+    const intermediateGain = audioCtx.createGain();
+    intermediateGain.gain.value = 0.85;
 
-        positionX: 320,
-        positionY: 100,
-        positionZ: 0,
-    });
-
-    osc.connect(panner);
-    panner.connect(gainNode);
+    osc.connect(reverb).connect(intermediateGain).connect(gainNode);
 };
 
 export const bassOscMod: OscModifier = (osc, audioCtx, gainNode) => {
     osc.type = "square";
 
     const compressor = audioCtx.createDynamicsCompressor();
+    compressor.threshold.value = -50;
+    compressor.ratio.value = 40;
+    compressor.attack.value = 0;
+    compressor.release.value = 0.25;
+
+    const makeupGain = audioCtx.createGain();
+    makeupGain.gain.value = 4.5;
 
     osc.connect(compressor);
-    compressor.connect(gainNode);
+    compressor.connect(makeupGain);
+    makeupGain.connect(gainNode);
 };
