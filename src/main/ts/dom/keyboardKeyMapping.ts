5e44f395bf91fa24ac426cc8514d94508abbc1d7
FelixRilling <contact@rilling.dev>
2020-09-20 12:03:54 +0200

    Allow keyboard controls.

---
+++ src/main/ts/dom/keyboardKeyMapping.ts
@@ -0,0 +1,79 @@
+import type { MidiEventHandler } from "../audio/midiEvent";
+import { Type } from "../audio/midiEvent";
+import type { Key } from "../audio/key";
+
+const qwertyKeyboardKeyMap = new Map<string, Key>([
+    ["1", { octave: 5, tone: "C" }],
+    ["2", { octave: 5, tone: "C#" }],
+    ["3", { octave: 5, tone: "D" }],
+    ["4", { octave: 5, tone: "D#" }],
+    ["5", { octave: 5, tone: "E" }],
+    ["6", { octave: 5, tone: "F" }],
+    ["7", { octave: 5, tone: "F#" }],
+    ["8", { octave: 5, tone: "G" }],
+    ["9", { octave: 5, tone: "G#" }],
+    ["0", { octave: 5, tone: "A" }],
+    ["-", { octave: 5, tone: "A#" }],
+    ["=", { octave: 5, tone: "B" }],
+
+    ["q", { octave: 4, tone: "C" }],
+    ["w", { octave: 4, tone: "C#" }],
+    ["e", { octave: 4, tone: "D" }],
+    ["r", { octave: 4, tone: "D#" }],
+    ["t", { octave: 4, tone: "E" }],
+    ["y", { octave: 4, tone: "F" }],
+    ["u", { octave: 4, tone: "F#" }],
+    ["i", { octave: 4, tone: "G" }],
+    ["o", { octave: 4, tone: "G#" }],
+    ["p", { octave: 4, tone: "A" }],
+    ["[", { octave: 4, tone: "A#" }],
+    ["]", { octave: 4, tone: "B" }],
+
+    ["a", { octave: 3, tone: "C" }],
+    ["s", { octave: 3, tone: "C#" }],
+    ["d", { octave: 3, tone: "D" }],
+    ["f", { octave: 3, tone: "D#" }],
+    ["g", { octave: 3, tone: "E" }],
+    ["h", { octave: 3, tone: "F" }],
+    ["j", { octave: 3, tone: "F#" }],
+    ["k", { octave: 3, tone: "G" }],
+    ["l", { octave: 3, tone: "G#" }],
+    [";", { octave: 3, tone: "A" }],
+    // These *could* be bound, but browsers usually bind them to something else.
+    // ["'", { octave: 3, tone: "A#" }],
+    // ["enter", { octave: 3, tone: "B" }],
+
+    ["z", { octave: 2, tone: "C" }],
+    ["x", { octave: 2, tone: "C#" }],
+    ["c", { octave: 2, tone: "D" }],
+    ["v", { octave: 2, tone: "D#" }],
+    ["b", { octave: 2, tone: "E" }],
+    ["n", { octave: 2, tone: "F" }],
+    ["m", { octave: 2, tone: "F#" }],
+    [",", { octave: 2, tone: "G" }],
+    [".", { octave: 2, tone: "G#" }],
+    // These *could* be bound, but browsers usually bind them to something else.
+    // ["/", { octave: 2, tone: "A" }],
+    // ["\\", { octave: 2, tone: "A#" }],
+    // ["`", { octave: 2, tone: "A" }],
+]);
+
+export const bindKeyboardKeyEvents = (
+    container: HTMLElement,
+    midiEventHandler: MidiEventHandler
+): void => {
+    const handleKeyEvent = (keyboardKey: string, type: Type): void => {
+        keyboardKey = keyboardKey.toLowerCase();
+        if (!qwertyKeyboardKeyMap.has(keyboardKey)) {
+            return;
+        }
+        const key = qwertyKeyboardKeyMap.get(keyboardKey)!;
+        midiEventHandler({ type, key });
+    };
+    container.addEventListener("keydown", (e) =>
+        handleKeyEvent(e.key, Type.PRESS)
+    );
+    container.addEventListener("keyup", (e) =>
+        handleKeyEvent(e.key, Type.RELEASE)
+    );
+};
--- src/main/ts/main.ts
+++ src/main/ts/main.ts
@@ -1,6 +1,5 @@
 import { getLogger } from "./logger";
 import { createStompClient } from "./messaging/stompClient";
-import type { Synth } from "./audio/synth";
 import { createSynth } from "./audio/synth";
 import { createPianoComponent } from "./dom/piano";
 import {
@@ -8,43 +7,62 @@ import {
     leadOscFactory,
     sineOscFactory,
 } from "./audio/oscFactory";
-import type { MidiEventClient } from "./messaging/midiEventClient";
 import { createDelegatingMidiEventClient } from "./messaging/midiEventClient";
+import { bindKeyboardKeyEvents } from "./dom/keyboardKeyMapping";
 
 const logger = getLogger("main");
 
 const pianoContainer1 = document.getElementById("piano1")!;
 const pianoContainer2 = document.getElementById("piano2")!;
 const pianoContainer3 = document.getElementById("piano3")!;
+const keyboardChannelSelect = document.getElementById(
+    "keyboardChannel"
+) as HTMLSelectElement;
 
 const synth1 = createSynth(sineOscFactory);
 const synth2 = createSynth(leadOscFactory);
 const synth3 = createSynth(bassOscFactory);
-
-const bindSocketSynth = (
-    client: MidiEventClient,
-    channel: number,
-    synth: Synth,
-    pianoContainer: HTMLElement
-): void => {
-    const piano = createPianoComponent(pianoContainer, (midiEvent) =>
-        client.publish(channel, midiEvent)
-    );
-
-    client.subscribe(channel, (midiEvent) => {
-        synth.handleMidiEvent(midiEvent);
-        piano.markPlayingStatus(midiEvent);
-    });
-};
 
 createStompClient(`wss://${location.host}${location.pathname}ws`)
     .then((rawClient) => {
         logger.info("Connected.", rawClient);
         const client = createDelegatingMidiEventClient(rawClient);
 
-        bindSocketSynth(client, 1, synth1, pianoContainer1);
-        bindSocketSynth(client, 2, synth2, pianoContainer2);
-        bindSocketSynth(client, 3, synth3, pianoContainer3);
+        const piano1 = createPianoComponent(pianoContainer1, (midiEvent) =>
+            client.publish(1, midiEvent)
+        );
+        client.subscribe(1, (midiEvent) => {
+            synth1.handleMidiEvent(midiEvent);
+            piano1.markPlayingStatus(midiEvent);
+        });
+
+        const piano2 = createPianoComponent(pianoContainer2, (midiEvent) =>
+            client.publish(2, midiEvent)
+        );
+        client.subscribe(2, (midiEvent) => {
+            synth2.handleMidiEvent(midiEvent);
+            piano2.markPlayingStatus(midiEvent);
+        });
+
+        const piano3 = createPianoComponent(pianoContainer3, (midiEvent) =>
+            client.publish(3, midiEvent)
+        );
+
+        client.subscribe(3, (midiEvent) => {
+            synth3.handleMidiEvent(midiEvent);
+            piano3.markPlayingStatus(midiEvent);
+        });
+
+        bindKeyboardKeyEvents(document.body, (midiEvent) => {
+            if (keyboardChannelSelect.value === "") {
+                return;
+            }
+            const channel = Number(keyboardChannelSelect.value);
+            if (Number.isNaN(channel)) {
+                return;
+            }
+            client.publish(channel, midiEvent);
+        });
 
         logger.info("Bound all synths.");
     })
