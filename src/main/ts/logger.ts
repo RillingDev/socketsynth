2be50625b8e407ac5ed70e1aa3e6c57e7728a1ef
FelixRilling <contact@rilling.dev>
2022-01-15 15:26:59 +0100

    Removed unused 3rd party packages.

--- src/main/ts/logger.ts
+++ src/main/ts/logger.ts
@@ -1,11 +1,19 @@
-import type { Logger } from "loglevel";
-import { getLogger as getLoglevelLogger, levels } from "loglevel";
-import { DEVELOPMENT_MODE } from "./mode";
+type LogFn = (...args: unknown[]) => void;
 
-const getLogger = (consumer: string): Logger => {
-	const logger = getLoglevelLogger(consumer);
-	logger.setLevel(DEVELOPMENT_MODE ? levels.DEBUG : levels.WARN);
-	return logger;
+interface Logger {
+	error: LogFn;
+	warn: LogFn;
+	info: LogFn;
+	debug: LogFn;
+	trace: LogFn;
+}
+
+export const getLogger = (name: string): Logger => {
+	return {
+		error: (...args) => console.error(name, ...args),
+		warn: (...args) => console.warn(name, ...args),
+		info: (...args) => console.info(name, ...args),
+		debug: (...args) => console.debug(name, ...args),
+		trace: (...args) => console.trace(name, ...args),
+	};
 };
-
-export { getLogger };
--- src/main/ts/mode.ts
+++
@@ -1,1 +0,0 @@
-export const DEVELOPMENT_MODE = process.env.NODE_ENV === "development";
