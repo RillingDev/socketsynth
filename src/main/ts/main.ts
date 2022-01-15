7f5894f0852a491157e2b76d98d6894d0b6f5c4c
FelixRilling <contact@rilling.dev>
2022-01-15 10:26:04 +0100

    Pull compat layer out of messaging client.

--- src/main/ts/main.ts
+++ src/main/ts/main.ts
@@ -7,8 +7,11 @@ import {
 	leadOscFactory,
 	sineOscFactory,
 } from "./audio/oscFactory";
-import { createDelegatingMidiEventClient } from "./messaging/midiEventClient";
+import type { MidiChannelMessage } from "./messaging/midiChannelMessageClient";
+import { createDelegatingMidiEventClient } from "./messaging/midiChannelMessageClient";
 import { bindKeyboardKeyEvents } from "./dom/keyboardKeyMapping";
+import type { Midi } from "./audio/midi";
+import { TONES } from "./audio/key";
 
 const logger = getLogger("main");
 
@@ -23,34 +26,71 @@ const synth1 = createSynth(sineOscFactory);
 const synth2 = createSynth(leadOscFactory);
 const synth3 = createSynth(bassOscFactory);
 
+const midiMessageToEvent = (
+	midiChannelMessage: MidiChannelMessage
+): [number, Midi] => {
+	const toneNum = midiChannelMessage.note % 12;
+	const tone = TONES[toneNum];
+	const octave = (midiChannelMessage.note - toneNum - 12) / 12;
+	const midi = {
+		command: midiChannelMessage.command,
+		key: {
+			octave,
+			tone,
+		},
+	};
+	return [midiChannelMessage.channel, midi];
+};
+
+const midiEventToMessage = (
+	channel: number,
+	data: Midi
+): MidiChannelMessage => {
+	const note = 12 + data.key.octave * 12 + TONES.indexOf(data.key.tone);
+	return {
+		command: data.command,
+		channel,
+		note,
+	};
+};
+
 createStompClient(`wss://${location.host}${location.pathname}ws`)
 	.then((rawClient) => {
 		logger.info("Connected.", rawClient);
 		const client = createDelegatingMidiEventClient(rawClient);
 
 		const piano1 = createPianoComponent(pianoContainer1, (midiEvent) =>
-			client.publish(1, midiEvent)
+			client.publish(midiEventToMessage(1, midiEvent))
 		);
-		client.subscribe(1, (midiEvent) => {
-			synth1.handleMidiEvent(midiEvent);
-			piano1.markPlayingStatus(midiEvent);
+		client.subscribe((message) => {
+			if (message.channel == 1) {
+				const [, event] = midiMessageToEvent(message);
+				synth1.handleMidiEvent(event);
+				piano1.markPlayingStatus(event);
+			}
 		});
 
 		const piano2 = createPianoComponent(pianoContainer2, (midiEvent) =>
-			client.publish(2, midiEvent)
+			client.publish(midiEventToMessage(2, midiEvent))
 		);
-		client.subscribe(2, (midiEvent) => {
-			synth2.handleMidiEvent(midiEvent);
-			piano2.markPlayingStatus(midiEvent);
+		client.subscribe((message) => {
+			if (message.channel == 2) {
+				const [, event] = midiMessageToEvent(message);
+				synth2.handleMidiEvent(event);
+				piano2.markPlayingStatus(event);
+			}
 		});
 
 		const piano3 = createPianoComponent(pianoContainer3, (midiEvent) =>
-			client.publish(3, midiEvent)
+			client.publish(midiEventToMessage(3, midiEvent))
 		);
 
-		client.subscribe(3, (midiEvent) => {
-			synth3.handleMidiEvent(midiEvent);
-			piano3.markPlayingStatus(midiEvent);
+		client.subscribe((message) => {
+			if (message.channel == 3) {
+				const [, event] = midiMessageToEvent(message);
+				synth3.handleMidiEvent(event);
+				piano3.markPlayingStatus(event);
+			}
 		});
 
 		bindKeyboardKeyEvents(document.body, (midiEvent) => {
@@ -61,7 +101,7 @@ createStompClient(`wss://${location.host}${location.pathname}ws`)
 			if (Number.isNaN(channel)) {
 				return;
 			}
-			client.publish(channel, midiEvent);
+			client.publish(midiEventToMessage(channel, midiEvent));
 		});
 
 		logger.info("Bound all synths.");
---
+++ src/main/ts/messaging/midiChannelMessageClient.ts
@@ -0,0 +1,88 @@
+import type { Client } from "@stomp/stompjs";
+import { getLogger } from "../logger";
+import { MidiCommand } from "../audio/midi";
+
+/**
+ * Simplified version of a MIDI message with fixed velocity.
+ */
+export interface MidiChannelMessage {
+	readonly command: MidiCommand;
+	readonly channel: number;
+	/**
+	 * MIDI notes are numbered from 0 to 127 assigned to C−1 to G9.
+	 */
+	readonly note: number;
+}
+
+// https://en.wikipedia.org/wiki/MIDI#Technical_specifications
+const serializeMidi = (midiChannelMessage: MidiChannelMessage): Uint8Array => {
+	const velocity = 0b1111111;
+
+	let commandNum: number;
+	if (midiChannelMessage.command == MidiCommand.NOTE_ON) {
+		commandNum = 0x90;
+	} else {
+		commandNum = 0x80;
+	}
+
+	const status = midiChannelMessage.channel | commandNum;
+
+	return new Uint8Array([status, midiChannelMessage.note, velocity]);
+};
+
+const deserializeMidi = (rawMidiMessage: Uint8Array): MidiChannelMessage => {
+	if (rawMidiMessage.length != 3) {
+		throw new TypeError("Invalid message.");
+	}
+	const status = rawMidiMessage[0];
+
+	const commandNum = status & 0xf0;
+	let command: MidiCommand;
+	if (commandNum == 0x90) {
+		command = MidiCommand.NOTE_ON;
+	} else if (commandNum == 0x80) {
+		command = MidiCommand.NOTE_OFF;
+	} else {
+		throw new TypeError("Unknown type.");
+	}
+
+	const channel = status & 0x0f;
+
+	const note = rawMidiMessage[1];
+
+	return {
+		command,
+		channel,
+		note,
+	};
+};
+
+export interface MidiChannelMessageClient {
+	readonly publish: (message: MidiChannelMessage) => void;
+	readonly subscribe: (
+		callback: (message: MidiChannelMessage) => void
+	) => void;
+}
+
+export const createDelegatingMidiEventClient = (
+	client: Client
+): MidiChannelMessageClient => {
+	const logger = getLogger("DelegatingMidiEventClient");
+	return {
+		publish: (message: MidiChannelMessage) => {
+			logger.debug(`Publishing: ${JSON.stringify(message)}.`);
+			client.publish({
+				destination: "/app/midi/input",
+				binaryBody: serializeMidi(message),
+			});
+		},
+		subscribe: (callback: (message: MidiChannelMessage) => void) => {
+			logger.debug(`Listening.`);
+			client.subscribe("/topic/midi/output", (frame) => {
+				const message = deserializeMidi(frame.binaryBody);
+				logger.debug(`Received: ${JSON.stringify(message)}.`);
+				callback(message);
+			});
+		},
+	};
+};
